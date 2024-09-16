cdf6072b546d28d911bbb65dcbfbbac4a6c3d59a
net-bob <jacklang0909@gmail.com>
2024-09-15 22:50:53 -0700

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/edwinsFunction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/edwinsFunction.java
@@ -1,9 +1,21 @@
+package it.polito.elite.teaching.cv;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Core;
+
 class edWinFunctions {
-    final int HSV_MAX = 180;
-    final int[] RANGES = new int[3];
-    RANGES[0] = {170, 10};
-    RANGES[1] = {10, 60};
-    RANGES[2] = {105, 135};
+    static final int HSV_MAX = 180;
+    int[][] RANGES = {{170, 10}, {10, 60}, {105, 135}};
+    // int[][] RANGES = new int[3][2];
+    // RANGES[0][0] = 170;
+    // RANGES[0][1] = 10;
+    // RANGES[1][0] = 10;
+    // RANGES[1][1] = 60;
+    // RANGES[2][0] = 105;
+    // RANGES[2][1] = 135;
     /*
      * 0: Red
      * 1: Yellow
@@ -11,21 +23,34 @@ class edWinFunctions {
      */
 
     public edWinFunctions(){
-
+        
     }
 
-    public static int[] get_ivals(int[] colorRange){
-        int[] ivals = new int[2];
-        if (colorRange.get(0) > colorRange.get(1)){
-            ivals[0] = new BetterTuples(colorRange.get(0), HSV_MAX);
-            ivals[1] = new BetterTuples(0, colorRange.get(1));
+    public static int[][] get_ivals(int[] colorRange){
+        int[][] ivals = new int[2][2];
+        if (colorRange[0] > colorRange[1]){
+            ivals[0][0] = colorRange[0];
+            ivals[0][1] = HSV_MAX;
+
+            ivals[1][0] = 0;
+            ivals[1][1] = colorRange[1];
         }
         else {
-            ivals[0] = colorRange;
+            ivals[0][0] = colorRange[0];
+            ivals[0][1] = colorRange[1];
         }
+        return ivals;
     }
 
-    public static getColor(){
+    public static int[][] transform_to_edwinArr(int[] ival){
+        int[][] mn_and_mx = new int[2][3];
+        int[] mn = {ival[0], 25, 25};
+        int[] mx = {ival[1], 255, 255};
+        mn_and_mx[0] = mn;
+        mn_and_mx[1] = mx;
 
+        return mn_and_mx;
     }
+
+    
 }
