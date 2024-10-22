a3b814569991a447e43ff46f3c4b35ccaf789231
otolba <omar16tolba@gmail.com>
2024-10-22 13:56:20 -0700

    n

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRServoTestTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRServoTestTele.java
@@ -5,26 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp(name="CRServoTele", group="Linear OpMode")
 public class CRServoTestTele extends RobotLinearOpMode {
-    private CRServo intake = null;
+    private CRServo intakeLeft = null;
+    private CRServo intakeRight = null;
 
     @Override
     public void runOpMode() {
-        intake = hardwareMap.get(CRServo.class, "intake");
-        intake.setPower(0);
+        intakeLeft = hardwareMap.get(CRServo.class, "intakeLeft");
+        intakeRight = hardwareMap.get(CRServo.class, "intakeRight");
+        intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        intakeLeft.setPower(0);
+        intakeRight.setPower(0);
         waitForStart();
 
         while(opModeIsActive()){
             if (gamepad1.a){
-                intake.setPower(1);
+                intakeLeft.setPower(1);
+                intakeRight.setPower(1);
             }
             if (gamepad1.b){
-                intake.setPower(-1);
+                intakeLeft.setPower(-1);
+                intakeRight.setPower(-1);
             }
             if (gamepad1.y){
-                intake.setPower(0);
+                intakeLeft.setPower(0);
+                intakeRight.setPower(0);
             }
         }
     }
