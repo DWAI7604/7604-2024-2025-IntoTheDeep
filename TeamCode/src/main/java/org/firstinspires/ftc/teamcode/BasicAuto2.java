4bcbd1e93da5ffdafd229e80c1b73beab6264bc9
AidanBOrger35 <aidan.g.schwartz@icloud.com>
2025-01-13 10:25:57 -0800

    Update BasicAuto2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto2.java
@@ -1,61 +1,109 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous
-public class BasicAuto2 extends RobotLinearOpMode{
+@Autonomous(name = "BasicAuto", group = "Linear Opmode")
+public class BasicAuto2 extends LinearOpMode {
+
+    private DcMotor leftFrontMotor;
+    private DcMotor rightFrontMotor;
+    private DcMotor leftBackMotor;
+    private DcMotor rightBackMotor;
+
     @Override
     public void runOpMode() {
+        // Initialize hardware
+        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
 
-        declareHardwareProperties();
+        // Set motor directions (adjust if needed based on your robot's configuration)
+        leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        while (!isStarted() && !isStopRequested()) {
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
 
-            declareAutoVariables();
+        if (opModeIsActive()) {
+            // Drive in a square
+            driveInSquare(0.5, 1000);
 
-            // Don't burn CPU cycles busy-looping in this sample
-            sleep(50);
+            // Drive in a circle
+            driveInCircle(0.5, 3000);
         }
-
-        while(opModeIsActive()){
-            sleep(waitTime);
-            if (!placeAndPark){
-                encoderDrive(0.4, 25, MOVEMENT_DIRECTION.REVERSE);
-                encoderSlideUp(0.7, 16, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.2, 4, MOVEMENT_DIRECTION.REVERSE);
-                encoderSlideUp(0.7, 5, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.4, 15, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.4, 16, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderTurn(0.8, 220, TURN_DIRECTION.TURN_RIGHT);
-                encoderDrive(0.4, 19, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.4, 3, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderSlideUpTime(0.7, 1.5, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.4, 18, MOVEMENT_DIRECTION.REVERSE);
-                sleep(200);
-
-                sleep(30000);
-            }
-            else{
-                encoderDrive(0.4, 25, MOVEMENT_DIRECTION.REVERSE);
-                encoderSlideUp(0.7, 16, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.2, 4, MOVEMENT_DIRECTION.REVERSE);
-                encoderSlideUp(0.7, 5, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.4, 15, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.4, 16, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderTurn(0.8, 220, TURN_DIRECTION.TURN_RIGHT);
-                encoderDrive(0.4, 19, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.4, 3, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderSlideUpTime(0.7, 1.5, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.4, 18, MOVEMENT_DIRECTION.REVERSE);
-                sleep(200);
-
-                sleep(30000);
-            }
-
-        }
-
     }
 
+    /**
+     * Drive in a square pattern.
+     * @param power The power level to set for the motors (0 to 1).
+     * @param duration The duration for each side of the square (in milliseconds).
+     */
+    public void driveInSquare(double power, int duration) {
+        for (int i = 0; i < 4; i++) {
+            driveStraight(power, duration);
+            turn90Degrees(power);
+        }
+    }
 
+    /**
+     * Drive in a circle pattern.
+     * @param power The power level to set for the motors (0 to 1).
+     * @param duration The duration to drive in the circle (in milliseconds).
+     */
+    public void driveInCircle(double power, int duration) {
+        leftFrontMotor.setPower(power);
+        rightFrontMotor.setPower(power * 0.5);
+        leftBackMotor.setPower(power);
+        rightBackMotor.setPower(power * 0.5);
+
+        sleep(duration);
+
+        stopMotors();
+    }
+
+    /**
+     * Drive straight for a specified duration.
+     * @param power The power level to set for the motors (0 to 1).
+     * @param duration The duration to drive straight (in milliseconds).
+     */
+    private void driveStraight(double power, int duration) {
+        leftFrontMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+        leftBackMotor.setPower(power);
+        rightBackMotor.setPower(power);
+
+        sleep(duration);
+
+        stopMotors();
+    }
+
+    /**
+     * Turn 90 degrees by running the motors in opposite directions for a short duration.
+     * @param power The power level to set for the motors (0 to 1).
+     */
+    private void turn90Degrees(double power) {
+        leftFrontMotor.setPower(power);
+        rightFrontMotor.setPower(-power);
+        leftBackMotor.setPower(power);
+        rightBackMotor.setPower(-power);
+
+        sleep(500); // Adjust duration based on your robot's turning characteristics
+
+        stopMotors();
+    }
+
+    /**
+     * Stop all motors.
+     */
+    private void stopMotors() {
+        leftFrontMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        leftBackMotor.setPower(0);
+        rightBackMotor.setPower(0);
+    }
 }
